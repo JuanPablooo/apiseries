4c98f03e41dc260b606635461104586bd2a9a01d
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-01-30 16:55:51 -0300

    gerando token e autenticando usuario

--- controllers/autenticacao.js
+++ controllers/autenticacao.js
@@ -1,6 +1,14 @@
 
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const UsuarioValidator = require('../validators/Usuario');
+const jwt = require('jsonwebtoken');
+const authConfig = require("../config/auth");
+gerarToken = (params)=> {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn:60
+
+    })
+}
 
 autenticacao = (app) =>{
     app.post("/registrar", 
@@ -8,15 +16,42 @@ autenticacao = (app) =>{
         (req, res)=>{
         const erros = validationResult(req);
         if(!erros.isEmpty()){
-            res.status(400).send(erros)
+            res.status(400).send({erros})
+
             return;
         }
         const usuario = req.body;
         usuarioDao = app.models.Usuarios;
         usuarioDao.insere(usuario)
-            .then(usuario=> res.status(201).send(usuario))
+            .then(usuario=> {
+                const token = gerarToken( {id:usuario.id});
+                res.status(201).send({usuario, token})
+            })
             .catch(erro => res.status(500).send(erro))
         
+    })
+    app.post('/autenticar', async (req, res)=>{
+        console.log("autenticando")
+        const {email, senha} = req.body;
+        try {
+
+            usuarioDao = app.models.Usuarios;
+            const usuario = await usuarioDao.buscarPorEmail(email);
+            if(!usuario){
+                console.log('uu')
+                return res.status(400).send({})
+            }
+            if(usuario.senha !== senha){
+                return res.status(400).send({erro: "senha invalida"})
+            }
+            const token = gerarToken( {id:usuario.id});
+
+            res.send({usuario, token})
+        } catch (erro) {
+            console.log(erro)
+            res.status(500).send(erro)
+            
+        }
     })
 }
 
